2f1c9832bc7af5ce74f78f65dd85b3eca3d68b5e
Gemma O'Connor <goconno2@pratt.edu>
2016-12-02 00:18:35 +0000

    footer move

--- product-page/js/main.js
+++ product-page/js/main.js
@@ -20,19 +20,19 @@ $(document).ready(function() {
                 $('#success-message').text("");
                 // $('#success-message').hide();
                 
-            }).done(function(data) {
-                console.log(data);
-                $('#success-message-bottom').text("Thank you!");
-                $('#error-message-bottom').text("");
-                // $('input[name-email]').val("");
+            // }).done(function(data) {
+            //     console.log(data);
+            //     $('#success-message-bottom').text("Thank you!");
+            //     $('#error-message-bottom').text("");
+            //     // $('input[name-email]').val("");
             
             
-            }).fail(function(data) {
-                console.log(data);
-                var errorMessage = JSON.parse(data.responseText).email[0];
-                $('#error-message-bottom').text('Oops! You submitted an invalid email address.');
-                $('#success-message-bottom').text("");
-                // $('#success-message').hide();
+            // }).fail(function(data) {
+            //     console.log(data);
+            //     var errorMessage = JSON.parse(data.responseText).email[0];
+            //     $('#error-message-bottom').text('Oops! You submitted an invalid email address.');
+            //     $('#success-message-bottom').text("");
+            //     // $('#success-message').hide();
             });
         event.preventDefault();
     });
